14e6c14ec679efff42b8bda9f4bd6a2050ffe431
Andrew Stewart <andrew@stwrt.ca>
2018-11-30 21:59:31 -0800

    Day 01, Elixir

--- elixir/01/lib/day1.ex
+++ elixir/01/lib/day1.ex
@@ -3,16 +3,32 @@ defmodule Day1 do
   Documentation for Day1.
   """
 
-  @doc """
-  Hello world.
+  def part1 do
+    input()
+    |> Enum.sum
+  end
 
-  ## Examples
+  def part2 do
+    input()
+    |> Stream.cycle()
+    |> Enum.reduce_while({0, MapSet.new()}, fn modulation, {frequency, set} ->
+      new_frequency = frequency + modulation
 
-      iex> Day1.hello()
-      :world
+      if MapSet.member?(set, new_frequency) do
+        {:halt, new_frequency}
+      else
+        {:cont, {new_frequency, MapSet.put(set, new_frequency)}}
+      end
+    end)
+  end
 
-  """
-  def hello do
-    :world
+  def input do
+    File.read!("../../rust/01/data")
+    |> String.trim()
+    |> String.split("\n")
+    |> Enum.map(&String.to_integer/1)
   end
 end
+
+IO.puts "Part 1 #{Day1.part1}"
+IO.puts "Part 2 #{Day1.part2}"
