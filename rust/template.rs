f953037ddc27b58a95da3d0b9b0545ad96a51eb0
Andrew Stewart <andrew@stwrt.ca>
2018-12-01 15:24:31 -0800

    Create Rust template

---
+++ rust/template.rs
@@ -0,0 +1,20 @@
+#![allow(dead_code, unused_imports, unused_variables)]
+
+use std::collections::{HashMap,HashSet};
+
+const DATA: &'static str = include_str!("../../../data/XX");
+
+type Input = &'static str;
+
+fn main() {
+    println!("Part 01: {}", part1(DATA));
+    println!("Part 02: {}", part2(DATA));
+}
+
+fn part1(input: Input) -> isize {
+    0
+}
+
+fn part2(input: Input) -> isize {
+    0
+}
