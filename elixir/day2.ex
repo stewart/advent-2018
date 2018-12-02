bda798ec98525f4b0af994a42aa466bede013f9f
Andrew Stewart <andrew@stwrt.ca>
2018-12-01 22:22:11 -0800

    Day 02, Elixir

--- elixir/day2.ex
+++ elixir/day2.ex
@@ -3,18 +3,49 @@ defmodule Day2 do
     input =
       File.read!("#{__DIR__}/../data/02")
       |> String.trim()
-      |> String.split("\n");
+      |> String.split("\n")
+      |> Enum.map(&String.graphemes/1)
 
-    IO.puts "Part 1 #{inspect(part1(input))}"
-    IO.puts "Part 2 #{inspect(part2(input))}"
+    IO.puts "Part 01: #{part1(input)}"
+    IO.puts "Part 02: #{part2(input)}"
   end
 
   def part1(input) do
-    0
+    counts =
+      input
+      |> Enum.map(fn chars ->
+        Enum.reduce(chars, %{}, fn ch, map ->
+          Map.update(map, ch, 1, &(&1 + 1))
+        end)
+      end)
+
+    twos =
+      counts
+      |> Enum.filter(fn map -> map |> Map.values() |> Enum.any?(&(&1 == 2)) end)
+      |> Enum.count()
+
+    threes =
+      counts
+      |> Enum.filter(fn map -> map |> Map.values() |> Enum.any?(&(&1 == 3)) end)
+      |> Enum.count()
+
+    twos * threes
   end
 
   def part2(input) do
-    0
+    for a <- input, b <- input do
+      if Enum.filter(Enum.zip(a, b), fn {x, y} -> x != y end) |> Enum.count() == 1 do
+        shared =
+          Enum.zip(a, b)
+          |> Enum.filter(fn {x, y} -> x == y end)
+          |> Enum.map(&elem(&1, 0))
+          |> Enum.join("")
+
+        throw {:break, shared}
+      end
+    end
+  catch
+    {:break, result} -> result
   end
 end
 
