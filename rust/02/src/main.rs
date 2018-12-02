d25595679e1d14f6955e072abfe28920e611f100
Andrew Stewart <andrew@stwrt.ca>
2018-12-01 21:14:15 -0800

    Day 02, Part 01

--- rust/02/src/main.rs
+++ rust/02/src/main.rs
@@ -11,8 +11,32 @@ fn main() {
     println!("Part 02: {}", part2(DATA));
 }
 
-fn part1(input: Input) -> isize {
-    0
+fn part1(input: Input) -> usize {
+    let values = input
+        .lines()
+        .map(|line| line.chars().collect())
+        .map(|chars: Vec<char>| {
+            let mut map = HashMap::new();
+
+            for ch in chars {
+                let entry = map.entry(ch).or_insert(0);
+                *entry += 1;
+            }
+
+            map
+        });
+
+    let number_of_values_with_two = values
+        .clone()
+        .filter(|map| map.values().any(|&value| value == 2))
+        .count();
+
+    let number_of_values_with_three = values
+        .clone()
+        .filter(|map| map.values().any(|&value| value == 3))
+        .count();
+
+    number_of_values_with_two * number_of_values_with_three
 }
 
 fn part2(input: Input) -> isize {
